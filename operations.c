8a263a2aa7ab7e636d4c51e04e1dd000715c8777
efan12114 <efanaddis1@gmail.com>
2026-08-06 12:08:21 +0000

    Fix f_push and data modes

--- operations.c
+++ operations.c
@@ -185,6 +185,204 @@ void f_push(stack_t **stack, unsigned int line_number)
                 else
                 {
                         while (temp->next)
+                        temp->next = new;
+                        new->prev = temp;
+                }
+        }
+}                                temp = temp->next;
+
+
+int is_digit(char *str)
+{
+        int i = 0;
+        if (!str)
+                return (0);
+        if (str[0] == '-')
+                i = 1;
+        for (; str[i]; i++)
+        {
+                if (str[i] < '0' || str[i] > '9')
+                        return (0);
+        }
+        return (1);
+}
+
+void f_push(stack_t **stack, unsigned int line_number)
+{
+        stack_t *new, *temp;
+        int n;
+
+        if (!global.arg || !is_digit(global.arg))
+        {
+                fprintf(stderr, "L%u: usage: push integer\n", line_number);
+                fclose(global.file);
+                free(global.line);
+                free_stack(*stack);
+                exit(EXIT_FAILURE);
+        }
+        n = atoi(global.arg);
+        new = malloc(sizeof(stack_t));
+        if (!new)
+        {
+                fprintf(stderr, "Error: malloc failed\n");
+                fclose(global.file);
+                free(global.line);
+                free_stack(*stack);
+                exit(EXIT_FAILURE);
+        }
+        new->n = n;
+        new->prev = NULL;
+        new->next = NULL;
+
+        if (global.data_mode == 0) /* Stack (LIFO) - Default */
+        {
+                new->next = *stack;
+                if (*stack)
+                        (*stack)->prev = new;
+                *stack = new;
+        }
+        else /* Queue (FIFO) */
+        {
+                temp = *stack;
+                if (!temp)
+                {
+                        *stack = new;
+                }
+                else
+                {
+                        while (temp->next)
+                                temp = temp->next;
+                        temp->next = new;
+                        new->prev = temp;
+                }
+        }
+}
+
+int is_digit(char *str)
+{
+        int i = 0;
+        if (!str)
+                return (0);
+        if (str[0] == '-')
+                i = 1;
+        for (; str[i]; i++)
+        {
+                if (str[i] < '0' || str[i] > '9')
+                        return (0);
+        }
+        return (1);
+}
+
+void f_push(stack_t **stack, unsigned int line_number)
+{
+        stack_t *new, *temp;
+        int n;
+
+        if (!global.arg || !is_digit(global.arg))
+        {
+                fprintf(stderr, "L%u: usage: push integer\n", line_number);
+                fclose(global.file);
+                free(global.line);
+                free_stack(*stack);
+                exit(EXIT_FAILURE);
+        }
+        n = atoi(global.arg);
+        new = malloc(sizeof(stack_t));
+        if (!new)
+        {
+                fprintf(stderr, "Error: malloc failed\n");
+                fclose(global.file);
+                free(global.line);
+                free_stack(*stack);
+                exit(EXIT_FAILURE);
+        }
+        new->n = n;
+        new->prev = NULL;
+        new->next = NULL;
+
+        if (global.data_mode == 0) /* Stack (LIFO) - Default */
+        {
+                new->next = *stack;
+                if (*stack)
+                        (*stack)->prev = new;
+                *stack = new;
+        }
+        else /* Queue (FIFO) */
+        {
+                temp = *stack;
+                if (!temp)
+                {
+                        *stack = new;
+                }
+                else
+                {
+                        while (temp->next)
+                                temp = temp->next;
+                        temp->next = new;
+                        new->prev = temp;
+                }
+        }
+}
+
+int is_digit(char *str)
+{
+        int i = 0;
+        if (!str)
+                return (0);
+        if (str[0] == '-')
+                i = 1;
+        for (; str[i]; i++)
+        {
+                if (str[i] < '0' || str[i] > '9')
+                        return (0);
+        }
+        return (1);
+}
+
+void f_push(stack_t **stack, unsigned int line_number)
+{
+        stack_t *new, *temp;
+        int n;
+
+        if (!global.arg || !is_digit(global.arg))
+        {
+                fprintf(stderr, "L%u: usage: push integer\n", line_number);
+                fclose(global.file);
+                free(global.line);
+                free_stack(*stack);
+                exit(EXIT_FAILURE);
+        }
+        n = atoi(global.arg);
+        new = malloc(sizeof(stack_t));
+        if (!new)
+        {
+                fprintf(stderr, "Error: malloc failed\n");
+                fclose(global.file);
+                free(global.line);
+                free_stack(*stack);
+                exit(EXIT_FAILURE);
+        }
+        new->n = n;
+        new->prev = NULL;
+        new->next = NULL;
+
+        if (global.data_mode == 0) /* Stack (LIFO) - Default */
+        {
+                new->next = *stack;
+                if (*stack)
+                        (*stack)->prev = new;
+                *stack = new;
+        }
+        else /* Queue (FIFO) */
+        {
+                temp = *stack;
+                if (!temp)
+                {
+                        *stack = new;
+                }
+                else
+                {
+                        while (temp->next)
                                 temp = temp->next;
                         temp->next = new;
                         new->prev = temp;
